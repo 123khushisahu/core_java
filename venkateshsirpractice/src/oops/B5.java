34f7d0dc13b7abd14016adc8215bbfe79d7d463d
123khushisahu <khushisahu9580@gmail.com>
2026-03-29 13:19:23 +0530

    this is my first time core java code i am doing commit.

---
+++ venkateshsirpractice/src/oops/B5.java
@@ -0,0 +1,13 @@
+package oops;
+abstract class A5{
+	static void print() {
+		System.out.println("WELCOME");
+	}
+}
+public class B5  {
+
+	public static void main(String[] args) {
+		A5.print();
+	}
+
+}
