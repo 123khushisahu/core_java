6ba569aae7967e47752ee90b7201cb1b5eef62f0
123khushisahu <khushisahu9580@gmail.com>
2026-04-06 17:44:58 +0530

    commit

---
+++ Interface/src/P1/InneerclassTest.java
@@ -0,0 +1,15 @@
+package P1;
+class Subclass1{
+	public void m1() {
+		System.out.println("Hello");
+	}
+}
+public class InneerclassTest {
+	
+	public static void main(String[] args) {
+		
+		Subclass1 s1 =new Subclass1();
+		s1.m1();
+	}
+
+}
